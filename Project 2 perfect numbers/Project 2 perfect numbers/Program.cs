72e81ad0dc8c02362d5a667dbcd16b12bbdb00db
AbdelrahmanSherif757 <121310025+AbdelrahmanSherif757@users.noreply.github.com>
2022-12-30 15:23:44 +0200

    Add files via upload

---
+++ Project 2 perfect numbers/Project 2 perfect numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+public class project
+{
+    public static void Main()
+    {
+        int n, i, sum;
+        int fristnumber , lastnumber;
+
+        Console.WriteLine("find perfect numbers\n");
+
+
+        Console.Write(" enter the frist number: ");
+         fristnumber = int.Parse(Console.ReadLine());
+
+        Console.Write("enter the last number : ");
+         lastnumber = int.Parse(Console.ReadLine());
+
+
+
+        for (n = fristnumber; n <= lastnumber; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("the perfect numbers :");
+                Console.WriteLine("{0} ", n);
+
+            }
+
+
+        }
+        Console.Write("\n");
+    }
+}
---
+++ project 2 prime numbers/project 2 prime numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter The Start Number: ");
+        int startNumber = int.Parse(Console.ReadLine());
+        Console.Write("Enter the End Number : ");
+        int endNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("prime numbers between " + startNumber + " and " + endNumber);
+
+        for (int i = startNumber; i <= endNumber; i++)
+        {
+            int counter = 0;
+
+            for (int j = 2; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    counter++;
+                    break;
+                }
+            }
+
+            if (counter == 0 && i != 1)
+            {
+                Console.WriteLine(i + " the prime number ");
+
+            }
+        }
+        
+    }
+}
+
